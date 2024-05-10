2da39f256963cbf73f5685f7b1151cd96ad5b2d9
C. Reimer <69945487+CJBR-97@users.noreply.github.com>
2024-05-10 11:50:35 -0400

    Update graph.py
    Added state_stacker

--- MatSimPy/graph.py
+++ MatSimPy/graph.py
@@ -3,6 +3,7 @@ from collections import Counter
 import networkx as nx
 import numpy as np
 import itertools
+import torch
 
 def init_graph_from_atm(atm):
     """
@@ -95,3 +96,44 @@ def gdegree(G):
     for r in range(len(G)+1):
         G_count[0][r] += (G_degree.count(r))
     return G_count
+
+
+def state_stacker(state_array, num_class, readout = True):
+  """
+  Produces arrays that contain information on uniqueness of state compositions for regions of interest with num_class distinct class numbers present
+
+  Parameters:
+  * state_array (array): A numpy array that acts as a dataset of ROI data containing types/classifications
+  * num_class (int): The number of different classes in the data that we are being asked to sort
+  Returns:
+  * unique_states (array): The unique states present in the input data
+  * unique_counts (array): Counts for unique states
+  * unique_firsts (array): Indices for first instances of unique states
+  * unique_inverse (array): The mapping needed to recreate the input data
+  """
+
+  print("States provided in np array of shape {}".format(state_array.shape))
+  # One-hot encode the torch tensor version of the ndarray and use the provided number of classes to classify them
+  # reduce values of all class numbers by 1 since one_hot() counts from 0 for classes
+  hot_state_array = one_hot(torch.tensor(state_array - 1), num_classes = num_class)
+  print("States now in shape {}".format(hot_state_array.shape))
+  # Take the sum across each state to get the total count per class for a single state as a row vector of length 5
+  hot_state_array = torch.sum(hot_state_array, 1)
+  print("States now in shape {}".format(hot_state_array.shape))
+  print(hot_state_array[0:10])
+  # Convert back to numpy
+  hot_state_array = hot_state_array.numpy()
+  # Get the unique rows and how often they occur
+  unique_states, unique_firsts, unique_inverse, unique_counts = np.unique(hot_state_array, axis = 0, return_counts = True, return_inverse = True, return_index = True)
+
+  if readout:
+    print("unique_states", unique_states.shape)
+    print(unique_states[0:10])
+    print("unique_counts", unique_counts.shape)
+    print(unique_counts[0:10])
+    print("unique_first", unique_firsts.shape)
+    print(unique_firsts[0:10])
+    print("unique_inverse", unique_inverse.shape)
+    print(unique_inverse[0:10])
+
+  return unique_states, unique_counts, unique_firsts, unique_inverse
