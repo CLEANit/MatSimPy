67cc3424521c430540b41bfe784f42112fa29cba
C. Reimer <69945487+CJBR-97@users.noreply.github.com>
2024-09-08 11:05:38 -0400

    Update ase.py

--- MatSimPy/ase.py
+++ MatSimPy/ase.py
@@ -2,29 +2,29 @@ from ase.build import add_vacuum
 from slist import uniques
 from itertools import chain
 
-# Removes vacuum from an ASE atoms object and centres the object
-def vacuum_adjust(ASE_Extracted, remove_vac):
+def vacuum_adjust(ASE_xtract, rm_vac):
     """
+    Removes vacuum from an ASE atoms object and centres the object.
     Parameters:
-    * ASE_Extracted (ASE atoms object): 
-    * remove_vac (float): A POSITIVE float value, the amount of vacuum to remove from the Atoms object
+    * ASE_xtract (ASE atoms object): 
+    * rm_vac (float): A positive float value, the amount of vacuum to remove from the Atoms object
     Returns: 
-    * ASE_Extracted (ASE Atoms object): the centred modified ASE atoms object
+    * ASE_xtract (ASE Atoms object): the centred modified ASE atoms object
     """
-    add_vacuum(ASE_Extracted, -remove_vac)
-    ASE_Extracted.center()
-    return ASE_Extracted  
+    add_vacuum(ASE_xtract, -rm_vac)
+    ASE_xtract.center()
+    return ASE_xtract  
   
 # Defines the composition of an ASE atoms object in a paired list output for chemical numbers and symbols
-def composition_identifier(Atoms_object):
+def composition_identifier(ASE_obj):
     """
     Parameters:
-    * Atoms_object (ASE atoms object): The molecule/atom to be analyzed
+    * ASE_obj (ASE atoms object): The molecule/atom to be analyzed
     Returns: 
     * [elem_list, num_list] (list): Paired lists of element symbols and element #'s'
     """
     entries = []
-    entries = [Atoms_object.get_chemical_symbols(), Atoms_object.get_atomic_numbers()]
+    entries = [ASE_obj.get_chemical_symbols(), ASE_obj.get_atomic_numbers()]
     elem_list = uniques(entries[0])
     num_list = uniques(entries[1])
     return [elem_list, num_list]  
